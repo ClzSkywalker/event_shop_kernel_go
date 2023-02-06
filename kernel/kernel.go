20a7e6162d6c6690c63fa16a1ca206806a57e283
Angular <1045683477@qq.com>
2023-02-06 22:51:42 +0800

    feat: mobile
    1.修改mobile启动

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -2,9 +2,10 @@ package kernel
 
 import (
 	"github.com/clz.skywalker/event.shop/kernal/internal/server"
+	"github.com/clz.skywalker/event.shop/kernal/pkg/utils"
 	_ "golang.org/x/mobile/bind"
 )
 
 func StartKernel() {
-	go server.ServerStart()
+	go utils.RecoverFunc(server.ServerStart)
 }
